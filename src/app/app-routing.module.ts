9e338eec2415e9b84a36bbe8548cbfad6e126e57
Matheus Felipe <matheusbarros9107@gmail.com>
2023-08-20 10:13:00 -0300

    header, footer e nav components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LivrosComponent } from './view/livros/livros.component';
 
 const routes: Routes = [];
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { HeaderComponent } from './components/template/header/header.component';
+import {MatButtonModule} from '@angular/material/button';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { LivrosComponent } from './view/livros/livros.component';
+import { NavComponent } from './components/template/nav/nav.component';
+import { CategoriasComponent } from './views/categorias/categorias.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LivrosComponent,
+    NavComponent,
+    CategoriasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
